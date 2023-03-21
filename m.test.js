2cfbb9e5c06aabe2b49e8e7469e5a3f7722f960a
Hisoka <gintama2009@hotmail.ca>
2023-03-21 23:41:43 +0000

    add Ship, Gameobard, and Player object and the tests for them

--- m.test.js
+++ m.test.js
@@ -1,5 +1,29 @@
-import { hello } from "./src/index.js";
+import { Ship, Gameboard, Player } from "./src/index.js";
 
-test("returns hello", () => {
-  expect(hello()).toBe("hello");
+test("Ship methods hit and isSunk work", () => {
+  const ship = Ship(1);
+  expect(ship.isSunk()).toBe(false);
+
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("Gameboard could report that all ships have sunk", () => {
+  const game = Gameboard();
+  game.placeShip(1, 4, 4, 0);
+
+  expect(game.isAllSunk()).toBe(false);
+  game.receiveAttack(4, 4);
+  expect(game.isAllSunk()).toBe(true);
+});
+
+test("Player could lose", () => {
+  const player = Player("player 1");
+  const game = Gameboard();
+  game.placeShip(1, 4, 4, 0);
+
+  expect(player.isLose(game)).toBe(false);
+
+  player.receiveAttack(4, 4, game);
+  expect(player.isLose(game)).toBe(true);
 });
