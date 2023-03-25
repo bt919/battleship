979c07fd44ec99e1adde50cbce61aa495be4e4d7
Hisoka <gintama2009@hotmail.ca>
2023-03-25 02:09:15 +0000

    done drag and drop ui, but need to fetch initial board pieces

--- src/display.js
+++ src/display.js
@@ -78,12 +78,53 @@ const displayInitialBoard = (element, game) => {
   const grid = document.createElement("div");
   grid.classList.add("board");
   grid.classList.add("playerBoard");
+  const ships = [];
   /** TODO - make all sea squares droppable and make all ships squares
    * draggable. When a ship square is dragged onto a sea square, they should switch
    * 'sea' and 'ship' class names, and in the board attribute, we should make the
    * necessary switch there also. We might even have to re-add appropriate event
    * listeners afterwards.
    */
+  function dragstart(e) {
+    e.dataTransfer.setData(
+      "text/plain",
+      `${e.target.id}|${e.target.classList[0]}|${e.target.textContent}`
+    );
+  }
+
+  function dragover(e) {
+    e.preventDefault();
+  }
+
+  function removeEventListeners(el) {
+    const newElement = el.cloneNode(true);
+    el.parentNode.replaceChild(newElement, el);
+  }
+
+  function drop(e) {
+    const data = e.dataTransfer.getData("text/plain").split("|");
+    const dragElement = document.querySelector(`#${data[0]}`);
+    const dropElement = document.querySelector(`#${e.target.id}`);
+    const temp = dropElement.classList[0];
+    dropElement.className = "";
+    dropElement.classList.add(`${data[1]}`);
+    dropElement.textContent = `${data[2]}`;
+    dragElement.textContent = "";
+    dragElement.className = "";
+    dragElement.classList.add(`${temp}`);
+    removeEventListeners(dragElement);
+    removeEventListeners(dropElement);
+    const nextDrop = document.querySelector(`#${dragElement.id}`);
+    const nextDrag = document.querySelector(`#${dropElement.id}`);
+    nextDrop.setAttribute("draggable", false);
+    nextDrag.setAttribute("draggable", true);
+    nextDrop.addEventListener("drop", (ev) => drop(ev));
+    nextDrop.addEventListener("dragover", (ev) => dragover(ev));
+    nextDrag.addEventListener("dragstart", (ev) => dragstart(ev));
+    // dragElement.addEventListener("drop", (ev) => drop(ev));
+    // dropElement.addEventListener("dragover", (ev) => dragover(ev));
+    // dropElement.addEventListener("dragstart", (ev) => dragstart(ev));
+  }
 
   for (let i = 0; i < board.length; i += 1) {
     for (let j = 0; j < board[i].length; j += 1) {
@@ -91,19 +132,20 @@ const displayInitialBoard = (element, game) => {
       const squareValue = board[i][j];
       const square = document.createElement("div");
       square.setAttribute("id", rank);
-      // square.classList.add(rank);
-      if (squareValue === 0) square.classList.add("sea");
-      else if (squareValue === 1) {
-        square.classList.add("shipHit");
-        square.textContent = "X";
-      } else if (squareValue === 2) {
-        square.classList.add("missedHit");
-        const circle = document.createElement("div");
-        circle.classList.add("circle");
-        square.appendChild(circle);
+      if (squareValue === 0) {
+        square.classList.add("sea");
+        square.addEventListener("dragover", (e) => dragover(e));
+        square.addEventListener("drop", (e) => drop(e));
       } else {
+        let index = ships.indexOf(squareValue);
+        if (index === -1) {
+          ships.push(squareValue);
+          index = ships.indexOf(squareValue);
+        }
+        square.textContent = `${index}`;
         square.classList.add("ship");
-        square.setAttribute("draggable", "true");
+        square.setAttribute("draggable", true);
+        square.addEventListener("dragstart", (e) => dragstart(e));
       }
 
       grid.appendChild(square);
--- src/index.js
+++ src/index.js
@@ -11,25 +11,3 @@ const board1 = document.querySelector(".board1");
 const board2 = document.querySelector(".board2");
 displayInitialBoard(board1, startGame);
 displayEnemyBoard(board2, startGame);
-
-const squares = document.querySelectorAll(".board1 > div");
-squares.forEach((square) => {
-  square.addEventListener("dragstart", (e) => {
-    e.dataTransfer.setData("text/plain", e.target.id);
-  });
-  square.addEventListener("dragenter", (e) => {
-    e.preventDefault();
-  });
-  square.addEventListener("dragover", (e) => {
-    e.preventDefault();
-  });
-  square.addEventListener("dragleave", (e) => {});
-  square.addEventListener("drop", (e) => {
-    // get the draggable element
-    const id = e.dataTransfer.getData("text/plain");
-    const draggable = document.getElementById(id);
-
-    // add it to the drop target
-    e.target.appendChild(draggable);
-  });
-});
